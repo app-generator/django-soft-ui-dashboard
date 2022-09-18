2bc2e9cdf61a5a90b85e140eb8205a336a9843ba
App Generator <51070104+app-generator@users.noreply.github.com>
2022-09-18 10:58:11 +0300

    Update helpers.py

--- apps/helpers.py
+++ apps/helpers.py
@@ -5,7 +5,6 @@ Copyright (c) 2019 - present AppSeed.us
 
 from functools import wraps
 
-from django.db import models
 from django.http import HttpResponseRedirect, HttpResponse
 
 def check_permission(function):
@@ -29,30 +28,3 @@ def check_permission(function):
         return function(viewRequest, *args, **kwargs)
 
     return wrap
-
-class Utils:
-    @staticmethod
-    def get_class(config, name: str) -> models.Model:
-        return Utils.model_name_to_class(config[name])
-
-    @staticmethod
-    def get_manager(config, name: str) -> models.Manager:
-        return Utils.get_class(config, name).objects
-
-    @staticmethod
-    def get_serializer(config, name: str):
-        class Serializer(serializers.ModelSerializer):
-            class Meta:
-                model = Utils.get_class(config, name)
-                fields = '__all__'
-
-        return Serializer
-
-    @staticmethod
-    def model_name_to_class(name: str):
-        all_classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
-        for cls in all_classes:
-            if cls[0] == name:
-                return cls[1]
-        # we are confident that never returns None
-        return None
