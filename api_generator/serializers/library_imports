da5f1b1b24d1c6acad3c45a8854897e1c8be586b
App Generator <51070104+app-generator@users.noreply.github.com>
2023-01-09 11:59:51 +0200

    CleanUP Before Codebase update

--- api_generator/serializers/library_imports
+++
@@ -1,1 +0,0 @@
-from rest_framework import serializers
--- apps/dyn_datatables/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DynDatatablesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.dyn_datatables'
