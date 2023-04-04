be94a6e0da43ce743da48e0f3e1684bafdcb8515
App Generator <51070104+app-generator@users.noreply.github.com>
2023-04-04 08:57:27 +0300

    Dummy

--- build.sh
+++ build.sh
@@ -4,11 +4,6 @@ set -o errexit
 
 python -m pip install --upgrade pip
 
-# recompile scss
-npm install gulp-cli
-npm i 
-gulp 
-
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
